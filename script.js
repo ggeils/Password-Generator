c806d190c131950593f979b7f63082578fc71204
George Geils <95538913+ggeils@users.noreply.github.com>
2022-02-22 01:14:16 -0500

    changed characters to arrays

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 var generateBtn = document.querySelector("#generate");
 
 // Character variables
-let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let lowerCase = "abcdefghijklmnopqrstuvwxyz";
-let numbers = "0123456789";
-let specialChars = "!@,#$%&*{}[]/\\+=";
+let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let specialChars = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
 let chosenChars = "";
 
 
@@ -15,7 +15,8 @@ function writePassword() {
   passwordText.value = password;
 }
 function generatePassword(){
-    var lengthChoice = prompt(" Please choose a length between 8-128 characters.")
+
+    let lengthChoice = prompt(" Please choose a length between 8-128 characters.")
 // display password in text box- write to page
 }
 // Add event listener to generate button
